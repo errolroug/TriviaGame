0d6d005a16c113092c0cdebbe1580a6917cb0d57
Errol Rougier <errolrougier@dyn-209-2-208-160.dyn.columbia.edu>
2018-10-27 10:17:21 -0400

    added the button

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+
+//declare some global variables such as time remaining
+
+//run run event that codes the game once start button is pressed
+$("#start").on("click", function(){
+
+var timeRemaining = 120
+var correctGuesses = 0
+var incorrectGuesses = 0
+//create a loop that will decrement the timeRemaining until it hits 0
+// add a timer using
+
+for(var i = 120 ; i < 120; i--){
+    
+}
+
+// when timer hits 0 make an event that displays correctGuesses and incorrectGuesses 
+//create a functions that allows user to choose one choice per line
+//create booleans for the answers true or false
+
+// create a function tests to see if answer is true or not
+
+//append child to empty divs for instructions content, and for actual trivia questions
+})
