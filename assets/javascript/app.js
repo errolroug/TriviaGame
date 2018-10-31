08e4fb34ae0aa7ba7f4eb41fdb746184986d344b
Errol Rougier <errolrougier@dyn-209-2-208-222.dyn.columbia.edu>
2018-10-31 18:59:26 -0400

    logic ive coded up until today

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,5 @@
 //when document is loaded, run function
-document.ready(function game(){
-
+// document.ready()
 //run event that starts the game once start button is pressed
 
 
@@ -9,57 +8,52 @@ var timeRemaining = 60;
 var correctGuesses = 0;
 var incorrectGuesses= 0;
 
-//create a boolean to use for when the gmae is running
-var gameRunning = false;
-
-
-
-//on click event to start the game
-$("#start-button").click(function(){
-    $("#start-button").hide();
-    var gameRunning = true;
-    
-});
-
 //create an array of Answers, and an array of questions
 
-var answersArray ={};
-var questionsArray={};
+var answersArray = [];
+var questionsArray=["What is another name for the title, Avatar The Last Airbender ?","How old is Aang in the show ?", "In the intro of Avatar : The Last Airbender, what is the third element Katara says ?","Who is the painted lady in episode 3 book 3 ?"]
+;
 
+//create a boolean to use for when the game is running
+var gameRunning = false;
 
 //create a timer  that will decrement the timeRemaining until it hits 0
 
-function Time(){
-    
-    timerCount = setInterval(timerDecrease, 1000);
-}
+function timer(){
+    timeRemaining--;
+    if(timeRemaing<= 0){
+        clearInterval(counter);
+        return;
+    };
 
-    function timerDecrease(){
-             timeRemaining-- ;
-           $("#timer").html(timeRemaining);
-    
-           if(timeRemaining === 0){
-      gameRunning = false;
-       clearInterval(timerDecrease)
-      $("#timer").html(timeRemaining)
-      $("#instructions").html()
-      }
-    }
-  
-})
+    $("#timer").html("Time left " + timeRemaining + " secs.");
 
+};
 
 //create a function to call that will display both Incorrectguesses and correct guesses
 
 function gameEnd(){
+    if(timeRemaining = 0){
     $("#game-over").html("Thats All Folks You have " + correctGuesses + " Answers Correct. You have " + incorrectGuesses + " Answers Incorrect")
+}
+};
+
+function triviaGame(){
+    //when guess is chosen display
+
 }
 
 
-// when timer hits 0 make an event that displays both values for guesses
-
-  
-//create a functions that allows user to choose one choice froim the array
+  //on click event to start the game
+$("#start-button").click(function (){
+    $(this).hide();
+    gameRunning = true;
+    counter = setInterval(timer,1000);
+    triviaGame();
+    timer();
+    
+});
+//create a functions that allows user to choose one choice from the array
 
 //create an if scenario if answers are correct
 
